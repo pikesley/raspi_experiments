dadcfbebb1c7f1c8f07229eadca518e00565f1fa
pikesley <sam@pikesley.org>
2014-12-28 18:27:33 +0000

    Random LED

--- python/flasher.py
+++ python/flasher.py
@@ -1,11 +1,12 @@
 import RPi.GPIO as GPIO  
 import time  
+import random  
 
-interval = 0.05
+interval = 0.01
 
 def flash(pin):
   GPIO.output(pin,GPIO.HIGH)  
-  time.sleep(interval) 
+  time.sleep(random.random() * interval) 
   GPIO.output(pin,GPIO.LOW)  
 
 GPIO.setmode(GPIO.BCM)  
@@ -14,8 +15,8 @@ pins = [23, 24, 25]
 for pin in pins:
   GPIO.setup(pin, GPIO.OUT)  
 
-for i in range(0, 1000):
-  for pin in pins:
-    flash(pin)
+for i in range(0, 10000):
+#  for pin in pins:
+  flash(random.choice(pins))
 
 GPIO.cleanup()   
