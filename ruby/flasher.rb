2400a3bcf40863a0673babe3742ea93030b3b1aa
pikesley <sam@pikesley.org>
2014-12-28 21:03:58 +0000

    Two LEDs

--- ruby/flasher.rb
+++ ruby/flasher.rb
@@ -2,11 +2,16 @@
 
 require 'pi_piper'
 
-pin = PiPiper::Pin.new(:pin => 18, :direction => :out)
+interval = 0.02
 
-100.times do
-  pin.on
-  sleep 0.1 
-  pin.off
-  sleep 0.1 
+pin_1 = PiPiper::Pin.new(:pin => 17, :direction => :out)
+pin_2 = PiPiper::Pin.new(:pin => 18, :direction => :out)
+
+1000000.times do
+  pin_1.on
+  sleep interval
+  pin_1.off
+  pin_2.on
+  sleep interval
+  pin_2.off
 end
